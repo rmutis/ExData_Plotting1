6b2065267ef6165a28cf7f97e1aa0fc48d2f434b
rmutis <r.mutis@outlook.de>
2022-07-24 10:20:48 +0200

    Bug-fixing in legend of plot 4

--- plot4.R
+++ plot4.R
@@ -35,7 +35,7 @@ with(data_filter, {
   plot(date_time, Sub_metering_1, xlab = "", ylab = "Energy sub metering", type = "l")
   points(date_time, Sub_metering_2, type = "l", col = "red")
   points(date_time, Sub_metering_3, type = "l", col = "blue")
-  legend("topright", pch = 1, col = c ("black", "red", "blue"), legend = c ("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  legend("topright", lty=1, lwd=2.5, col = c ("black", "red", "blue"), legend = c ("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
   plot(date_time, Global_reactive_power, xlab = "datetime", ylab = "Global_reactive_power", type = "l")
   })
 dev.off()
